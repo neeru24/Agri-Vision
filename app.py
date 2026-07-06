8605869fa056e8c3c388c7a3854d95fa5bbfe2c8
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-06 15:32:17 +0530

    feat: add view analysis report option (#872)

--- app.py
+++ app.py
@@ -3989,3 +3989,4 @@ def api_admin_permissions():
         return jsonify({"status": "success", "id": perm.id})
     perms = Permission.query.all()
     return jsonify([{"id": p.id, "name": p.name, "slug": p.slug} for p in perms])
+# TODO: feat: add view analysis report option (#872)
