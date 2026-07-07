36884f5f34e3f6ab4e8c15b0e23bd26d8ed86469
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-07 15:58:03 +0530

    [Bug] API 404 errors return HTML templates instead of JSON responses (#639)

--- app.py
+++ app.py
@@ -3989,3 +3989,5 @@ def api_admin_permissions():
         return jsonify({"status": "success", "id": perm.id})
     perms = Permission.query.all()
     return jsonify([{"id": p.id, "name": p.name, "slug": p.slug} for p in perms])
+
+# TODO: fix for #639: [Bug] API 404 errors return HTML templates instead of JSON responses
