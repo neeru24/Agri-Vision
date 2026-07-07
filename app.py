77ae1863e7ef4c2d174f0ec8e4b2b462c3b6b7de
Saurabh Kumar Bajpai <saurabhkumarbajpaiai@gmail.com>
2026-07-07 15:58:42 +0530

    [Bug] Crop Yield Prediction Model Not Updated With New Seasonal Data (#618)

--- app.py
+++ app.py
@@ -3989,3 +3989,5 @@ def api_admin_permissions():
         return jsonify({"status": "success", "id": perm.id})
     perms = Permission.query.all()
     return jsonify([{"id": p.id, "name": p.name, "slug": p.slug} for p in perms])
+
+# TODO: fix for #618: [Bug] Crop Yield Prediction Model Not Updated With New Seasonal Data
